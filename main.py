bafae56227de45e32d8e38d1e159b5369ab1b205
Christopher Bradshaw <christopher.peter.bradshaw+github@gmail.com>
2017-12-04 21:53:20 -0800

    end of sunday

--- main.py
+++ main.py
@@ -11,18 +11,27 @@ def cell_mean(arr):
 def norm(arr):
     return arr / np.mean(arr)
 
+def get_positions():
+    # np.linspace(0, self.width, num=self.nParticles+1) # defined on t
+    return np.concatenate((
+        np.arange(0, 40, 2),
+        np.arange(40, 60, 1/3),
+        np.arange(60, 101, 2),
+    ))
 class OneDFluid():
-    gamma = 5/3
+    gamma = 7/3
 
     nParticles = 100
     width = 100
     deltaT = 1
     massParticle = 1
+    work = np.array([])
 
     def __init__(self):
         # Things that are defined on the cell boundaries
             # and defined on time boundaries
-        self.positions = np.linspace(0, self.width, num=self.nParticles+1) # defined on t
+        self.positions = get_positions()
+
             # and defined on time + 1/2
         self.velocities = np.random.normal(0, 0.01, len(self.positions)) # defined of t+1/2
         # hard boundary
@@ -33,10 +42,11 @@ class OneDFluid():
             # and defined on time boundaries
         self.cell_size = np.ediff1d(self.positions)
         self.densities = self.massParticle / self.cell_size
-        self.energies = 0.5 * self.densities * np.power(cell_mean(self.velocities), 2)
+        self.energies = np.ones_like(self.densities) # just start with some energy (this is the temperature)
         self.pressures = self.get_pressures()
 
         # sanity check
+        self.plot()
         self.sanity_checks()
 
     def get_pressures(self):
@@ -50,41 +60,67 @@ class OneDFluid():
         self.deltaT = self.get_time_step()
 
         # Move things defined at t up to t+1
+            # first the basics
         self.positions += self.velocities * self.deltaT
         self.cell_size = np.ediff1d(self.positions)
         self.densities = self.massParticle / self.cell_size
+
+            # now energies and pressures
+            # diff(velocity) > 0 = expansion
+        self.work = np.ediff1d(self.velocities) * self.deltaT * self.pressures
+        self.energies -= self.work
         self.pressures = self.get_pressures()
 
         # Move things defined at t+1/2 up to t+3/2
-            # something is probably slightly wrong here. A sign. Might also need an energy term?
-        self.velocities[1:-1] -= self.deltaT * np.ediff1d(self.pressures) / np.ediff1d(self.densities)
+            # something is probably slightly wrong here. A sign. Might also need a work term?
+        pressureDV = np.ediff1d(self.pressures) * self.deltaT / np.ediff1d(self.densities)
+        workDV = np.ediff1d(self.work) * self.deltaT / np.ediff1d(self.densities)
+        self.velocities[1:-1] -= pressureDV + workDV
 
 
         # Now plot and sanity check
-        self.sanity_checks()
         self.plot()
+        self.sanity_checks()
 
     def plot(self):
         _, ax = plt.subplots()
-        ax.plot(norm(self.densities), label="Density")
-        ax.plot(norm(self.pressures), label="Pressure")
-        ax.plot(norm(self.energies), label="Energy")
+        ax.plot(self.densities, label="Density")
+        ax.plot(self.velocities, label="Velocities")
+        # ax.plot(self.pressures, label="Pressure")
+        # ax.plot(self.energies, label="Energy")
         ax.legend()
         plt.show()
 
     def sanity_checks(self):
         try:
-            assert np.all(np.ediff1d(self.positions) == self.cell_size)
-            assert np.all(np.ediff1d(self.positions) > 0)
-            assert (self.velocities[0] == 0 and self.velocities[-1] == 0)
-            assert self.deltaT > 0
-            print(np.sum(self.energies))
-        except:
-            print(self.positions)
-            print(self.velocities)
-            print(np.ediff1d(self.positions))
-            raise
+            # Position assertions
+            assert np.all(self.densities * self.cell_size) == self.massParticle, "Mass is conserved"
+            assert np.sum(self.cell_size) == 100, "Total width is conserved"
+            assert np.all(np.ediff1d(self.positions) > 0), "Cells never overlap"
+            assert np.all(np.ediff1d(self.positions) == self.cell_size), "We aren't messing up this calc"
 
+            # Velocity assertions
+            assert (self.velocities[0] == 0 and self.velocities[-1] == 0)
+
+            # Time assertions
+            assert self.deltaT > 0
+
+            # Energy assertions
+            if len(self.work):
+                assert np.isclose(np.sum(self.work), 0), "No net work"
+            assert np.isclose(np.sum(self.energies), 100), "Energy is conserved"
+
+            print("Success!")
+        except:
+            print(np.sum(self.energies * self.densities))
+            print(self.cell_size)
+            print(np.sum(self.cell_size))
+            # print(np.sum(self.work))
+            # print(np.sum(self.densities))
+            # print(self.positions)
+            # print(self.velocities)
+            # print(np.ediff1d(self.positions))
+            raise
 
 def main():
     x = OneDFluid()
