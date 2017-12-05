1c76cd16518c00fd153f079f1659d502bf3c95fe
Christopher Bradshaw <christopher.peter.bradshaw+github@gmail.com>
2017-12-04 23:17:04 -0800

    WORKING :wookie:

--- lib/v2.py
+++ lib/v2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import numpy as np
+import matplotlib.pyplot as plt
 
 def getPressure(energy, volume, gamma):
     return energy * (gamma - 1) / volume
@@ -38,7 +39,7 @@ class OneDFluid():
         self.gaps["pressure"] = getPressure(self.gaps["energy"], self.gaps["volume"], self.gamma)
 
     def getDeltaT(self):
-        return 1
+        return 0.00001
 
     def evolve(self):
         deltaT = self.getDeltaT()
@@ -56,6 +57,24 @@ class OneDFluid():
                 deltaT * newGrid["velocity"]
         )
 
+        # Pull volume from t = 0 to t = 1
+        newGaps["volume"] = self.initialRho * (
+                np.ediff1d(newGrid["position"]) / self.dx
+        )
+        # Also define this deltaV for each gap
+        deltaV = newGaps["volume"] - self.gaps["volume"]
+
+        # Get the vorticity in the middle of this timestep
+        # I don't think this is pulling? But see comment for my thoughts on vorticity
+        newGaps["vorticity"] = np.zeros(self.width) # Fuck vorticity for now
+
+        # Pull energy from t = 0 to t = 1
+        newGaps["energy"] = self.gaps["energy"] - (
+                (self.gaps["pressure"] - self.gaps["vorticity"]) * deltaV
+        )
+
+        # Pull pressure from t = 0 to t = 1
+        newGaps["pressure"] = getPressure(newGaps["energy"], newGaps["volume"], self.gamma)
 
         self.grid = newGrid
         self.gaps = newGaps
@@ -73,9 +92,24 @@ class OneDFluid():
 
 def main():
     x = OneDFluid()
-    print(x)
-    x.evolve()
-    print(x)
+    i = 0
+
+    fig, ax = plt.subplots()
+    volLine, = ax.plot(x.gaps["volume"], label="Cell Volumes")
+    energyLine, = ax.plot(x.gaps["energy"], label="Cell Energies")
+    pressureLine, = ax.plot(x.gaps["pressure"], label="Cell Pressures")
+    plt.legend()
+    plt.show(block=False)
+
+    while True:
+        if i % 1000 == 0:
+            print(x)
+            volLine.set_ydata(x.gaps["volume"])
+            energyLine.set_ydata(x.gaps["energy"])
+            pressureLine.set_ydata(x.gaps["pressure"])
+            fig.canvas.draw()
+        x.evolve()
+        i += 1
 
 if __name__ == "__main__":
     main()
